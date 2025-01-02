3fce45b7d1f621eb3ad1f73f2efbf744e5b6e3f8
Parida <parida.lp@gmail.com>
2025-01-02 22:21:39 +0100

    exo refacto

--- cypress/e2e/pages/MainPage.js
+++ cypress/e2e/pages/MainPage.js
@@ -3,9 +3,23 @@ class MainPage {
     cy.visit("/");
   }
 
+  verifyUrl(){
+    cy.url().should(
+      "eq",
+      "https://spring-framework-petclinic-qctjpkmzuq-od.a.run.app/"
+    );
+  }
+
+  verifyNavbar(){
+    cy.get(".navbar").should("exist");
+    cy.get(".navbar a").should("have.length.at.least", 4).and("be.visible");
+    cy.get(".navbar a").contains("Home").click();
+  }
+
   verifyPageElements() {
     cy.get(".img-responsive").should("have.attr", "src").and("contain", ".png");
     cy.get("h2").should("contain", "Welcome");
+    cy.get(".fa-home").should("exist");
   }
 
   navigateToFindOwners() {
